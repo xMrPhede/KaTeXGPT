08b5f0f86f358a1edc1c2f54e5b92ab793d3815c
Federico Granata <xmrphede@gmail.com>
2025-02-14 11:24:17 +0100

    Fixed GPT not working

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.action.onClicked.addListener((tab) => {
   if (
-    tab.url.includes("chat.openai.com") ||
+    tab.url.includes("chatgpt.com/") ||
     tab.url.includes("chat.deepseek.com")
   ) {
     chrome.tabs.sendMessage(tab.id, { action: "PROMPT" });
