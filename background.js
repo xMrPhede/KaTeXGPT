ec0b27d0bd9cb48e0047c182fba8a3503e73fa90
Federico <64491992+xMrPhede@users.noreply.github.com>
2025-10-23 02:07:16 +0200

    Added support for perplexity, claude and gemini

--- background.js
+++ background.js
@@ -1,7 +1,10 @@
 chrome.action.onClicked.addListener((tab) => {
   if (
     tab.url.includes("chatgpt.com/") ||
-    tab.url.includes("chat.deepseek.com")
+    tab.url.includes("chat.deepseek.com") ||
+    tab.url.includes("perplexity.ai") ||
+    tab.url.includes("claude.ai") ||
+    tab.url.includes("gemini.google.com") 
   ) {
     chrome.tabs.sendMessage(tab.id, { action: "PROMPT" });
   }
